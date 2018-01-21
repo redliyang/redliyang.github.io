582f840fc0d05f3b77cd888cf31568afca429524
redliyang <lixusheng_php@qq.com>
2018-01-21 20:24:58 +0800

    1

--- src/js/obj.js
+++ src/js/obj.js
@@ -2,23 +2,29 @@ function Marray(arr) {
     this.arr = arr;
 }
 Marray.prototype = {
-    constructor : Marray,
-    push : function (params) {
+    constructor: Marray,
+    push: function (params) {
         this.arr[this.arr.length] = params;
         return this.arr.length;
     },
-    pop : function () {
+    pop: function () {
         var temp = this.arr[this.arr.length - 1];
         this.arr.length--;
         return temp;
     }
 }
 
-       function Person(){
-           this.name = "王九"
-       }
+function Person() {
+    this.name = "王九"
+}
 
-       Person.prototype.name = "张三";
+function PersonPP() {
+    this.name = "王九"
+}
 
-       var p = new Person();
-       console.log(p.valueOf());
+Person.prototype = new String();
+Person.prototype.constructor = Person();
+Person.prototype = new String();
+
+var p = new Person();
+console.log(p);
