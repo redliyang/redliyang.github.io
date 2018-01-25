6ca18b105ef783eb6eac8d9904780faed23fd590
redliyang <lixusheng_php@qq.com>
2018-01-25 17:54:59 +0800

    1

--- src/js/obj.js
+++ src/js/obj.js
@@ -1,28 +1,79 @@
-function Marray(arr) {
-    this.arr = arr;
-}
-Marray.prototype = {
-    constructor: Marray,
-    push: function (params) {
-        this.arr[this.arr.length] = params;
-        return this.arr.length;
-    },
-    pop: function () {
-        var temp = this.arr[this.arr.length - 1];
-        this.arr.length--;
-        return temp;
-    }
-}
+// function Marray(arr) {
+//     this.arr = arr;
+// }
+// Marray.prototype = {
+//     constructor: Marray,
+//     push: function (params) {
+//         this.arr[this.arr.length] = params;
+//         return this.arr.length;
+//     },
+//     pop: function () {
+//         var temp = this.arr[this.arr.length - 1];
+//         this.arr.length--;
+//         return temp;
+//     }
+// }
 // 
 //
 
 
-var num = 123;
-function f1() {
-    console.log(num);  //如果是动态作用域打印的就是456 如果是词法作用域 打印123
+//1
+//        var age = 38;
+//        var obj = {
+//            age: 18,
+//            getAge: function() {
+//                console.log(this.age);
+//            }
+//        };
+
+// //        var a = obj.getAge();  //18
+//        var getAge = obj.getAge;
+//        getAge();
+
+
+// //2
+//        var age = 38;
+//        var obj = {
+//            age: 18,
+//            getAge: function() {
+
+//             //    console.log(this.age);
+
+//                function foo() {
+//                    console.log(this.age); //
+//                }
+//                foo();
+//            }
+//        };
+
+//        obj.getAge();
+
+//3
+//
+function Person(name, age) {
+    var o = {
+        name: name,
+        age: age,
+        sayHello: function () {
+
+        }
+    }
+    return o;
 }
-function f2() {
-    var num = 456;
-    f1();
+
+var p = Person("张三", 18);
+console.log(p);
+
+function Person3(name, age) {
+    var o = {
+        name: name,
+        age: age,
+        sayHello: function () {
+
+        }
+    }
+    return o;
 }
-f2();
+
+var p1 = new Person3();
+console.log(p1);
