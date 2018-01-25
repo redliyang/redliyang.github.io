1e118cf900f901e1b65aa48085e81d851d7fd309
redliyang <lixusheng_php@qq.com>
2018-01-25 19:55:14 +0800

    1

--- src/js/obj.js
+++ src/js/obj.js
@@ -17,15 +17,23 @@
 //
 
 
-function Person() {
-    this.name = "张莎";
-    this.age = 18;
+var div = document.getElementById("div1");
+
+function huidiao(e) {
+    console.log(e + "就是当前的事件对象")
+    console.log(this + "就是当前你点击的对象");
+    alert("我是div的点击事件，所有浏览器中，我的功能都是弹出来这句话！");
 }
 
-function Student() {
-    var stu = this;
-    Person.apply(stu);
-}
+            //通用的
+        //    div.onclick = huidiao;
 
-var stu = new Student();
-console.log(stu);
+            //ie9以上才支持
+        //    div.addEventListener("click",huidiao)
+
+            //ie9（不包括9）以下支持的
+           div.attachEvent("onclick",function(){
+               huidiao.call(div, window.event);
+
+
+           })
