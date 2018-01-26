a8d9e9e5e415669af24b13aceb8430ddc28813c9
redliyang <lixusheng_php@qq.com>
2018-01-26 08:41:42 +0800

    1

--- src/js/obj.js
+++ src/js/obj.js
@@ -14,26 +14,3 @@
 //     }
 // }
 // 
-//
-
-
-var div = document.getElementById("div1");
-
-function huidiao(e) {
-    console.log(e + "就是当前的事件对象")
-    console.log(this + "就是当前你点击的对象");
-    alert("我是div的点击事件，所有浏览器中，我的功能都是弹出来这句话！");
-}
-
-            //通用的
-        //    div.onclick = huidiao;
-
-            //ie9以上才支持
-        //    div.addEventListener("click",huidiao)
-
-            //ie9（不包括9）以下支持的
-           div.attachEvent("onclick",function(){
-               huidiao.call(div, window.event);
-
-
-           })
