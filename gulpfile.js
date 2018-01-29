d372e68a72c9c584313509db6c0ccd95b68b227f
redliyang <lixusheng_php@qq.com>
2018-01-29 17:38:30 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-var gulp = require('gulp'),
-    changed = require('gulp-changed'),//检查改变状态  
-    browserSync = require("browser-sync").create();//浏览器实时刷新  
+let gulp = require('gulp')
+let changed = require('gulp-changed')
+let browserSync = require('browser-sync').create()
 
-// //删除dist下的所有文件  
+// //删除dist下的所有文件
 // gulp.task('delete', function (cb) {
 //     return del(['dist/*', '!dist/images'], cb);
 // })
 
-// //压缩html  
+// //压缩html
 // gulp.task('html', function () {
 //     var options = {
-//         removeComments: true,//清除HTML注释  
-//         collapseWhitespace: true,//压缩HTML  
-//         removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"  
-//         removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"  
-//         minifyJS: true,//压缩页面JS  
-//         minifyCSS: true//压缩页面CSS  
+//         removeComments: true,//清除HTML注释
+//         collapseWhitespace: true,//压缩HTML
+//         removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+//         removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+//         minifyJS: true,//压缩页面JS
+//         minifyCSS: true//压缩页面CSS
 //     };
 //     gulp.src('src/*.html')
 //         .pipe(changed('dist', { hasChanged: changed.compareSha1Digest }))
@@ -24,14 +24,14 @@ var gulp = require('gulp'),
 //         .pipe(browserSync.reload({ stream: true }));
 // });
 
-// //实时编译less  
+// //实时编译less
 // // gulp.task('less', function () {
-// //     gulp.src(['./src/less/*.less']) //多个文件以数组形式传入  
+// //     gulp.src(['./src/less/*.less']) //多个文件以数组形式传入
 // //         .pipe(changed('dist/css', { hasChanged: changed.compareSha1Digest }))
-// //         .pipe(less())//编译less文件  
-// //         .pipe(concat('index.css'))//合并之后生成index.css  
-// //         .pipe(cleanCSS())//压缩新生成的css  
-// //         .pipe(gulp.dest('dist/css'))//将会在css下生成main.css  
+// //         .pipe(less())//编译less文件
+// //         .pipe(concat('index.css'))//合并之后生成index.css
+// //         .pipe(cleanCSS())//压缩新生成的css
+// //         .pipe(gulp.dest('dist/css'))//将会在css下生成main.css
 // //         .pipe(browserSync.reload({ stream: true }));
 // // });
 
@@ -40,13 +40,13 @@ var gulp = require('gulp'),
 //         .pipe(sourcemaps.init())
 //         .pipe(changed('dist/css', { hasChanged: changed.compareSha1Digest }))
 //         .pipe(less()) //该任务调用的模块
-//         .pipe(cleanCSS())//压缩新生成的css  
+//         .pipe(cleanCSS())//压缩新生成的css
 //         .pipe(sourcemaps.write())
 //         .pipe(gulp.dest('dist/css')) //将会在src/css下生成index.css
 //         .pipe(browserSync.reload({ stream: true }));
 // });
 
-// //压缩js  
+// //压缩js
 // gulp.task("script", function () {
 //     gulp.src(['src/js/*.js'])
 //         .pipe(changed('dist/js', { hasChanged: changed.compareSha1Digest }))
@@ -56,63 +56,63 @@ var gulp = require('gulp'),
 //         .pipe(browserSync.reload({ stream: true }));
 // });
 
-// // 压缩图片  
+// // 压缩图片
 // gulp.task('images', function () {
 //     gulp.src('./src/images/*.*')
 //         .pipe(changed('dist/images', { hasChanged: changed.compareSha1Digest }))
 //         .pipe(imageMin({
-//             progressive: true,// 无损压缩JPG图片  
-//             svgoPlugins: [{ removeViewBox: false }], // 不移除svg的viewbox属性  
-//             use: [pngquant()] // 使用pngquant插件进行深度压缩  
+//             progressive: true,// 无损压缩JPG图片
+//             svgoPlugins: [{ removeViewBox: false }], // 不移除svg的viewbox属性
+//             use: [pngquant()] // 使用pngquant插件进行深度压缩
 //         }))
 //         .pipe(gulp.dest('dist/images'))
 //         .pipe(browserSync.reload({ stream: true }));
 // });
 
-// //启动热更新  
+// //启动热更新
 // gulp.task('serve', ['delete'], function () {
 //     gulp.start('script', 'less', 'html');
 //     browserSync.init({
 //         port: 2017,
 //         server: {
-//             baseDir: ['dist'] 
+//             baseDir: ['dist']
 //         }
 //     });
-//     gulp.watch('src/js/*.js', ['script']);         //监控文件变化，自动更新  
-//     gulp.watch('src/less/**/*.less', ['less']);
-//     gulp.watch('src/*.html', ['html']);
-//     gulp.watch('src/images/*.*', ['images']);
+//     gulp.watch('src/js/*.js', ['script'])
+//     gulp.watch('src/less/**/*.less', ['less'])
+//     gulp.watch('src/*.html', ['html'])
+//     gulp.watch('src/images/*.*', ['images'])
 // });
 
 gulp.task('html', function () {
     gulp.src('src/*.html')
         .pipe(changed('dist', { hasChanged: changed.compareSha1Digest }))
         .pipe(gulp.dest('dist'))
-        .pipe(browserSync.reload({ stream: true }));
-});
+        .pipe(browserSync.reload({ stream: true }))
+})
 
-gulp.task("script", function () {
+gulp.task('script', function () {
     gulp.src(['src/js/*.js'])
         .pipe(changed('dist/js', { hasChanged: changed.compareSha1Digest }))
         .pipe(gulp.dest('dist/js'))
-        .pipe(browserSync.reload({ stream: true }));
-});
+        .pipe(browserSync.reload({ stream: true }))
+})
 
 gulp.task('serve', function () {
-    gulp.start('script','html');
+    gulp.start('script', 'html')
     browserSync.init({
-        port: 2017,
+        port: 2018,
         server: {
             baseDir: ['dist']
         }
-    });
-    gulp.watch('src/js/*.js', ['script']);         //监控文件变化，自动更新  
-    gulp.watch('src/less/**/*.less', ['less']);
-    gulp.watch('src/*.html', ['html']);
-    gulp.watch('src/images/*.*', ['images']);
-});
+    })
+    gulp.watch('src/js/*.js', ['script'])
+    gulp.watch('src/less/**/*.less', ['less'])
+    gulp.watch('src/*.html', ['html'])
+    gulp.watch('src/images/*.*', ['images'])
+})
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve'])
 
 // {
 //     "name": "gulp",
