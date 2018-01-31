fd211615da384da5764173513d0afdf942f25b87
redliyang <lixusheng_php@qq.com>
2018-01-31 15:06:40 +0800

    1

--- src/js/event.js
+++ src/js/event.js
@@ -29,90 +29,41 @@ $.fn.extend({
         jQuery.each(this, (key, dom) => {
             const self = dom
             self.$_event_cache = self.$_event_cache || {}
-            const selfEventCache = self.$_event_cache
-            if (!selfEventCache[type]) {
-                selfEventCache[type] = []
-                selfEventCache[type].push(fn)
+            if (!self.$_event_cache[type]) {
+                self.$_event_cache[type] = []
+                self.$_event_cache[type].push(fn)
                 jQuery.addEvent(self, type, (e) => {
-                    jQuery.each(selfEventCache[type], (k, fun) => {
+                    jQuery.each(self.$_event_cache[type], (k, fun) => {
                         fun.call(self, e)
                     })
                 })
             } else {
-                selfEventCache[type].push(fn)
+                self.$_event_cache[type].push(fn)
             }
         })
         return this
     },
     off(type, fn) {
+        const argLen = arguments.length
         jQuery.each(this, (key, dom) => {
             const self = dom
-            if (!this.$_event_cache) {
-                // return false
-            } else if (arguments.length === 0) {
-                for (var key in this.$_event_cache) {
-                    this.$_event_cache[key] = []
+            if (!self.$_event_cache) {
+                // return this
+            } else if (argLen === 0) {
+                for (const key in self.$_event_cache) {
+                    self.$_event_cache[key] = []
                 }
-            } else if (arguments.length === 1) {
+            } else if (argLen === 1) {
                 self.$_event_cache[type] = []
-            } else {
-                for (let i = this.$_event_cache[type].length - 1; i >= 0; i--) {
-                    if (this.$_event_cache[type][i] === fn) {
-                        this.$_event_cache[type].splice(i, 1)
+            } else if (argLen === 2) {
+                for (let i = self.$_event_cache[type].length - 1; i >= 0; i -= 1) {
+                    if (self.$_event_cache[type][i] === fn) {
+                        self.$_event_cache[type].splice(i, 1)
                     }
                 }
             }
-            // jQuery.removeEvent(self, type, fn)
         })
         return this
-
-        // var argLen = arguments.length;
-        // this.each(function () {
-        //     // 没有绑定过任何事件，就不用处理了
-        //     if (!this.$_event_cache) {
-        //         return;
-        //     }
-        //     // 如果绑过事件，需要进一步处理
-        //     else {
-
-        //         // 如果没有传参，遍历所有的事件数组，分别清空
-        //         if (argLen === 0) {
-        //             for (var key in this.$_event_cache) {
-        //                 this.$_event_cache[key] = [];
-        //             }
-        //         }
-
-        //         // 如果传如一个参数，则清空指定事件类型的数组
-        //         else if (argLen === 1) {
-        //             this.$_event_cache[type] = [];
-        //         }
-
-        //         // 如果传入多个参数，则清空指定事件类型数组中指定的回调函数
-        //         else {
-
-        //             // 遍历对应事件类型的数组，得到每一个回调
-        //             for (var i = this.$_event_cache[type].length - 1; i >= 0; i--) {
-
-        //                 // 依次和传入的回调比较，如果相等，则从数组中剔除
-        //                 if (this.$_event_cache[type][i] === fn) {
-        //                     this.$_event_cache[type].splice(i, 1);
-        //                 }
-        //             }
-        //         }
-        //     }
     },
 })
-function clickHandle() {
-    alert('span')
-    console.log(this.value)
-}
-function clickHandle2() {
-    alert('spanspanspanspan')
-}
-$('input').on('click', clickHandle).on('click', clickHandle2).on('click', clickHandle2)
-    .off()
-// const arr = []
-// arr.push(clickHandle)
-// arr.push(clickHandle)
-// arr.push(clickHandle2)
-// console.log(arr)
+
