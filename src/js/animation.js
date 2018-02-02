4777b194168479bfc846e11e7f63d0e81fdfcc41
redliyang <lixusheng_php@qq.com>
2018-02-02 16:34:05 +0800

    1

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,63 +1,31 @@
 $.fn.extend({
-    say(blinkDom, content = '大家好！我是李阳', time = 400) {
-        let num = 0
-        let str = ''
-        const self = this
-        const $blinkDom = $(blinkDom)
-        const arr = content.split('')
-        $blinkDom.css('display', 'inline')
-        const timer = setInterval(() => {
-            if (arr[num] === undefined) {
-                clearInterval(timer)
-                $('.typedCursor')[0].style.display = 'none'
-            } else {
-                str += arr[num]
-                self.text(str)
-                num += 1
-            }
-        }, time)
-        //  display: none; opacity: 1; animation: blink .7s infinite;
-        //  @keyframes blink { 0%, 100% { opacity: 1 } 50% { opacity: 0 } }
-    },
-    show() {
-        return this.css('display', 'block')
-    },
-    hide() {
-        return this.css('display', 'none')
-    },
+    height: '',
+    display: '',
+    opacity: '', // 兼容IE678 值取 10 - 100
     animate: function animate(json, fn) {
         const ele = this[0]
         // 先清定时器
         clearInterval(ele.timer)
         ele.timer = setInterval(() => {
-            // 开闭原则
             let bool = true
-            // 遍历属性和值，分别单独处理json
             // attr == k(键)    target == json[k](值)
             jQuery.each(json, (attr, target) => {
-                // 四部
+                // opacity 乘100 方便运算
                 let leader = (attr === 'opacity') ?
                     (jQuery.getStyle(ele, attr) * 100 || 1) :
                     (parseInt(jQuery.getStyle(ele, attr), 10) || 0)
-                // 判断如果属性为opacity的时候特殊获取值
-                // if (attr === 'opacity') {
-                //     leader = jQuery.getStyle(ele, attr) * 100 || 1
-                // } else {
-                //     leader = parseInt(jQuery.getStyle(ele, attr)) || 0
-                // }
                 // 1.获取步长
                 let step = (target - leader) / 10
                 // 2.二次加工步长
                 step = step > 0 ? Math.ceil(step) : Math.floor(step)
                 leader += step
                 // 3.赋值
-                // 特殊情况特殊赋值
+                // 分别对 opacity zIndex 特殊处理
+                // opacity ÷100 zIndex一次性赋值
                 if (attr === 'opacity') {
                     ele.style[attr] = leader / 100
                     // 兼容IE678
                     ele.style.filter = `alpha(opacity=${leader})`
-                    // 如果是层级，一次行赋值成功，不需要缓动赋值
-                    // 为什么？需求！
                 } else if (attr === 'zIndex') {
                     ele.style.zIndex = target
                 } else {
@@ -83,5 +51,121 @@ $.fn.extend({
         }, 25)
         return this
     },
-
+    show() {
+        const self = this
+        return self.css('display', $.fn.display)
+    },
+    hide() {
+        const self = this
+        // 使用原型属性 保存DOM原来对 display模式
+        $.fn.display = self.css('display')
+        return self.css('display', 'none')
+    },
+    toggle() {
+        const self = this
+        return self.css('display') === 'none' ?
+            self.show() : self.hide()
+    },
+    slideDown() {
+        const self = this
+        if (self.css('display') === 'block') {
+            return self
+        }
+        self.show()
+        self.animate({ height: $.fn.height }, () => self)
+        return self
+    },
+    slideUp() {
+        const self = this
+        if (self.css('display') === 'none') {
+            return self
+        }
+        // 使用原型属性 保存DOM原来对 height(不加px)
+        $.fn.height = parseInt(self.css('height'), 10)
+        self.animate({ height: 0 }, () => {
+            self.hide()
+        })
+        return self
+    },
+    slideToggle() {
+        return this.css('display') === 'none' ?
+            this.slideDown() : this.slideUp()
+    },
+    fadeIn() {
+        const self = this
+        if (self.css('display') === 'none') {
+            return self
+        }
+        // 使用原型属性 保存DOM原来对 兼容IE678 值取 10 - 100
+        $.fn.opacity = self.css('opacity') * 100
+        self.animate({ opacity: 0 }, () => {
+            self.hide()
+        })
+        return self
+    },
+    fadeOut() {
+        const self = this
+        if (self.css('display') === 'block') {
+            return self
+        }
+        self.show()
+        self.animate({ opacity: $.fn.opacity }, () => self)
+        return self
+    },
+    fadeTo(val, fn) {
+        return this.animate({ opacity: val }, fn)
+    },
+    fadeToggle() {
+        return this.css('display') === 'none' ?
+            this.fadeOut() : this.fadeIn()
+    },
+    say(blinkDom, content = '大家好！我是李阳', time = 400) {
+        let num = 0
+        let str = ''
+        const self = this
+        const $blinkDom = $(blinkDom)
+        const arr = content.split('')
+        $blinkDom.css('display', 'inline')
+        const timer = setInterval(() => {
+            if (arr[num] === undefined) {
+                clearInterval(timer)
+                $('.typedCursor')[0].style.display = 'none'
+            } else {
+                str += arr[num]
+                self.text(str)
+                num += 1
+            }
+        }, time)
+        //  display: none; opacity: 1; animation: blink .7s infinite;
+        //  @keyframes blink { 0%, 100% { opacity: 1 } 50% { opacity: 0 } }
+    },
 })
+
+const json1 = {
+    left: 300,
+    top: 200,
+    width: 500,
+    height: 200,
+    opacity: 30,
+    zIndex: 1,
+}
+
+$('#a1').click(() => {
+    console.log($('div').fadeIn(json1))
+    // $('button').slideDown()
+})
+$('#a2').click(() => {
+    console.log($('div').fadeOut(json1))
+    // $('button').slideDown()
+})
+$('#a3').click(() => {
+    console.log($('div').fadeToggle(json1))
+    // $('button').slideDown()
+})
+$('#a4').click(() => {
+    console.log($('div').fadeTo(100))
+    // $('button').slideDown()
+})
+// console.log($('button').hide().show())
+// // $('button').slideDown()
+// console.log(1)
