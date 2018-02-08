70da86b593051692acf998e3e47825ac4f1cc937
redliyang <lixusheng_php@qq.com>
2018-02-08 08:57:18 +0800

    1

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,15 +1,1 @@
-function foo() {
-    var num = Math.random();
-    function func() {
-        return num;
-    }
-    return func;
-}
-
-
-var f = foo();
-// f可以直接访问num，而且多次访问，访问的也是同一个，并不会返回新的num
-var res1 = f();
-var res2 = f();
-console.log(res1)
-console.log(res2)
+console.log(1)
